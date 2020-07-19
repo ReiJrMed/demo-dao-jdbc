0e70060b1610a58247d40f5515a40a8aab4298d6
ReiJrMed <reijrspy@gmail.com>
2020-07-19 13:18:13 -0300

    Criada a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,30 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "Livros");
+		try {
 		
-		System.out.println(dp);
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		    
+			Department dp = new Department(1, "Livros");
+		    
+			Seller seller = new Seller(21, "Bob", "Bob@gmail.com", sdf.parse("22/08/1988"), 3000.0, dp);
+		    
+		    System.out.println(seller);
+        
+		} catch (ParseException e) {
+			
+			e.printStackTrace();
+		}
+
 
 	}
 
